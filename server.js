d49e0bae7d2b7401160ad4f202566abaab0355bc
ush189 <git@ush189.de>
2022-11-26 19:54:16 +0100

    change hello world output

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Hello Node-World!');
 });
 
 app.get('/badminton', function (req, res) {
